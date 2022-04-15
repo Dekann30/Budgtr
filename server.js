5d1ce459a43339c450ec5dfc4174f7f748b08f11
Deanna K <keysdeanna@gmail.com>
2022-04-15 12:23:44 -0400

    adds normalize and skeleton css files to index.ejs

--- server.js
+++ server.js
@@ -5,8 +5,20 @@ const app = express()
 const PORT = process.env.PORT || 3001
 const budget = require('./models/budget.js')
 
+//Middleware
+app.use(express.urlencoded({extended:false}))
+app.use('/static', express.static('public'))
+
 app.get('/', (req,res)=>{
-    res.send('Hello World')
+    res.send('<h1>My Budget App</h1>')
 })
+
+//Index
+
+//New
+
+//Create
+
+//Show
 
 app.listen(PORT, ()=>{console.log("It's ALIVE~")})
